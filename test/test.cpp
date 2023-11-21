043adc2a7a32ad01d6540755cf4983db20967ed9
KshitijKarnawat <kshitijkarnawat009@gmail.com>
2023-11-20 19:05:01 -0500

    Push for TA Checking

--- test/test.cpp
+++ test/test.cpp
@@ -11,119 +11,65 @@
 #include <rclcpp/rclcpp.hpp>
 #include <gtest/gtest.h>
 #include <stdlib.h>
-
+#include <beginner_tutorials/srv/string_change.hpp>
 #include <std_msgs/msg/string.hpp>
-
+#include <geometry_msgs/msg/transform_stamped.hpp>
+#include <tf2/LinearMath/Quaternion.h>
+#include <tf2_ros/buffer.h>
+#include <tf2_ros/transform_broadcaster.h>
+#include <tf2_ros/transform_listener.h>
+#include "rclcpp/rclcpp.hpp"
+#include "rclcpp_action/rclcpp_action.hpp"
+#include <chrono>
+#include <memory>
 class TaskPlanningFixture : public testing::Test {
  public:
-  TaskPlanningFixture()
-      : node_(std::make_shared<rclcpp::Node>("basic_test"))
-  {
-    RCLCPP_INFO_STREAM(node_->get_logger(), "DONE WITH CONSTRUCTOR!!");
-  }
 
   void SetUp() override {
-    // Setup things that should occur before every test instance should go here
+    rclcpp::init(0, nullptr);
 
-    /*
-     * 1.) Define any ros2 package and exectuable you want to test
-     *  example: package name = cpp_pubsub, node name = minimal_publisher, executable = talker
-     */
-    bool retVal = StartROSExec ("minimal_integration_test", "minimal_publisher", "talker");
-    ASSERT_TRUE(retVal);
+    node_ = std::make_shared<rclcpp::Node>("test");
+    client_ = node_->create_client<beginner_tutorials::srv::StringChange>(
+        "string_change");
 
-    RCLCPP_INFO_STREAM(node_->get_logger(), "DONE WITH SETUP!!");
-
+    while (!client_->wait_for_service(1s)) {
+      if (!rclcpp::ok()) {
+        RCLCPP_FATAL_STREAM(
+            node_->get_logger(),
+            "Interrupted while waiting for the service. Exiting.");
+        exit(1);
+      }
+      RCLCPP_INFO_STREAM(node_->get_logger(),
+                         "service not available, waiting again...");
+    }
   }
 
   void TearDown() override {
-    // Tear things that should occur after every test instance should go here
-
-    // Stop the running ros2 node, if any.
-    bool retVal = StopROSExec ();
-    ASSERT_TRUE(retVal);
-
-    std::cout << "DONE WITH TEARDOWN" << std::endl;
+    rclcpp::shutdown();
   }
 
  protected:
   rclcpp::Node::SharedPtr node_;
-  std::stringstream cmd_ss, cmdInfo_ss, killCmd_ss;
-
-  bool StartROSExec (const char* pkg_name,
-                     const char* node_name,
-                     const char* exec_name)
-  {
-    // build command strings
-    cmd_ss << "ros2 run " << pkg_name << " " << exec_name << " > /dev/null 2> /dev/null &";
-    cmdInfo_ss << "ros2 node info " << "/" << node_name << " > /dev/null 2> /dev/null";
-    char execName[16];  snprintf (execName, 16, "%s", exec_name); // pkill uses exec name <= 15 char only
-    killCmd_ss << "pkill --signal SIGINT " << execName << " > /dev/null 2> /dev/null";
-
-    // First kill the ros2 node, in case it's still running.
-    StopROSExec();
-    
-    // Start a ros2 node and wait for it to get ready:
-    int retVal =  system (cmd_ss.str().c_str());
-    if (retVal != 0)
-      return false;
-    
-    retVal = -1;
-    while (retVal != 0) {
-      retVal = system (cmdInfo_ss.str().c_str());
-      sleep (1);
-    }
-    return true;
-  }
-
-  bool StopROSExec ()
-  {
-    if (killCmd_ss.str().empty())
-      return true;
-    
-    int retVal =  system (killCmd_ss.str().c_str());
-    return retVal == 0;
-  }
-  
+  rclcpp::Client<beginner_tutorials::srv::StringChange>::SharedPtr client_;
 };
 
 TEST_F(TaskPlanningFixture, TrueIsTrueTest) {
-  std::cout << "TEST BEGINNING!!" << std::endl;
-  EXPECT_TRUE(true);
+  EXPECT_EQ(1, 1);
+}
 
-  /*
-   * 2.) subscribe to the topic 
-   */
-  using std_msgs::msg::String;
-  using SUBSCRIBER = rclcpp::Subscription<String>::SharedPtr;
-  bool hasData = false;
-  SUBSCRIBER subscription = node_->create_subscription<String>
-    ("topic", 10,
-     // Lambda expression begins
-     [&](const std_msgs::msg::String& msg) {
-       RCLCPP_INFO(node_->get_logger(), "I heard: '%s'", msg.data.c_str());
-       hasData = true;
-     } // end of lambda expression
-     );
 
-  /*
-   * 3.) check to see if we get data winhin 3 sec
-   */
-  using timer = std::chrono::system_clock;
-  using namespace std::chrono_literals;
-  timer::time_point clock_start;
-  timer::duration elapsed_time;
-  clock_start = timer::now();
-  elapsed_time = timer::now() - clock_start;
-  rclcpp::Rate rate(2.0);       // 2hz checks
-  while (elapsed_time < 3s)
-    {
-      rclcpp::spin_some(node_);
-      rate.sleep();
-      elapsed_time = timer::now() - clock_start;
-    }
-  EXPECT_TRUE (hasData);
-  
+TEST_F(TestPub, testTF) {
+  auto tfBuffer = std::make_shared<tf2_ros::Buffer>(node_->get_clock());
+  auto listener = std::make_shared<tf2_ros::TransformListener>(*tfBuffer);
+  geometry_msgs::msg::TransformStamped transformStamped;
+  try {
+    transformStamped =
+        tfBuffer->lookupTransform("world", "talk", rclcpp::Time(0), 5s);
+  } catch (tf2::TransformException &ex) {
+    RCLCPP_ERROR(node_->get_logger(), "Failure %s ", ex.what());
+  }
+  ASSERT_EQ(transformStamped.header.frame_id, "world");
+  ASSERT_EQ(transformStamped.child_frame_id, "talk");
 }
 
 int main(int argc, char** argv) {
